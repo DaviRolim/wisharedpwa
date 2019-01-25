75becba81337cca8c9f880c0a8b963c07f8e9237
davi.holanda <davi.holanda@mv.com.br>
2019-01-25 09:30:26 -0300

    a simple drawer example

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <v-app>
     <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up" ></v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase text-center">
+        <span>WISHARED</span>
+        <!-- <span class="font-weight-light">MATERIAL DESIGN</span> -->
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
@@ -14,7 +15,41 @@
         <span class="mr-2">Latest Release</span>
       </v-btn>
     </v-toolbar>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="mini"
+      absolute
+      temporary
+    >
+      <v-list class="pa-1">
+        <v-list-tile avatar tag="div">
+          <v-list-tile-avatar>
+            <img src="https://randomuser.me/api/portraits/men/85.jpg">
+          </v-list-tile-avatar>
 
+          <v-list-tile-content>
+            <v-list-tile-title>John Leider</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
+      <v-list class="pt-0" dense>
+        <v-divider light></v-divider>
+
+        <v-list-tile
+          v-for="item in items"
+          :key="item.title"
+        >
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
     <v-content>
       <HelloWorld/>
     </v-content>
@@ -31,6 +66,12 @@ export default {
   },
   data () {
     return {
+      drawer: null,
+      items: [
+        { title: 'Home', icon: 'dashboard' },
+        { title: 'About', icon: 'question_answer' }
+      ],
+      right: null
       //
     }
   }
